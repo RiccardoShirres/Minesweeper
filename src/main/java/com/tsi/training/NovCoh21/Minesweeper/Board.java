f1c6654dc3f6dc2721c86e8232f2d2f999d7e396
ricca <rshirres@softwareinstitute.com>
2021-11-22 10:50:46 +0000

    Boom3

---
+++ src/main/java/com/tsi/training/NovCoh21/Minesweeper/Board.java
@@ -0,0 +1,69 @@
+package com.tsi.training.NovCoh21.Minesweeper;
+
+import java.util.ArrayList;
+
+public class Board {
+
+    ////////////////////////Attributes///////////////////
+
+    public int size;
+
+    public String difficulty;
+
+    public int totalMines;
+
+    public ArrayList grid;
+
+    ////////////////////////Methods//////////////////////
+
+    public void createGrid(int size)
+    {
+        ArrayList<ArrayList<Space>> grid = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+        {
+            grid.add(new ArrayList<Space>(size));
+        }
+        this.grid = grid;
+    }
+
+    ///////////////////////Constructor//////////////////
+
+    public Board(int size)
+    {
+        createGrid(size);
+    }
+
+    ////////////////////////Getters & Setters////////////
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public String getDifficulty()
+    {
+        return difficulty;
+    }
+
+    public int getTotalMines()
+    {
+        return totalMines;
+    }
+
+    public ArrayList getGrid() {return grid;}
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public void setDifficulty(String difficulty)
+    {
+        this.difficulty = difficulty;
+    }
+
+    public void setTotalMines(int totalMines)
+    {
+        this.totalMines = totalMines;
+    }
+}
